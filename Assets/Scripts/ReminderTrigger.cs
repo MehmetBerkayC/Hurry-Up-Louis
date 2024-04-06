e02549b92b1153288b0365a706e1885dcd96c088
Mehmet Berkay Coruk <beqostore@gmail.com>
2024-04-06 17:58:23 +0300

    Everything done up until Kitchen

--- Assets/Scripts/ReminderTrigger.cs
+++ Assets/Scripts/ReminderTrigger.cs
@@ -7,12 +7,12 @@ public class ReminderTrigger : MonoBehaviour
     [SerializeField]
     private Dialogue[] reminderDialogues; // 0 bathroom 1 kitchen
 
-    [SerializeField]
-    private Collider2D trigger;
+    [SerializeField] private Collider2D trigger;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     public static ReminderTrigger Instance;
 
-    public bool IsInteractable { get; private set; }
+    public bool IsKitchenDone { get; private set; } = false;
 
     private void Awake()
     {
@@ -28,5 +28,35 @@ public class ReminderTrigger : MonoBehaviour
 
     private void Start()
     {
+        TryGetComponent(out spriteRenderer);
+        TryGetComponent(out trigger);
+        ActivateTrigger(false);
+    }
+
+    public void ActivateTrigger(bool value)
+    {
+        trigger.enabled = value;
+        spriteRenderer.enabled = value;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsKitchenDone)
+        {
+            // Play Bathroom Reminder
+            DialogueManager.Instance.StartDialogue(reminderDialogues[0]);
+        }
+        else
+        {
+            // Play Kitchen Reminder
+            DialogueManager.Instance.StartDialogue(reminderDialogues[1]);
+        }
+
+        ActivateTrigger(false);
+    }
+
+    public void SetIsKitchenDone(bool value)
+    {
+        IsKitchenDone = value;
     }
 }
